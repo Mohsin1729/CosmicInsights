a1a4058c1ae718e280d2efcc83e20d17adcaf035
Sk Mohammed Mohasin <123063081+Mohsin1729@users.noreply.github.com>
2024-01-29 13:22:06 +0530

    Add files via upload
    Hohman orbit transfer Mars class work

---
+++ constants(1).py
@@ -0,0 +1,48 @@
+# Screen dimensions
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1020
+
+# Astronomical Unit
+AU = 1.496 * 10**11
+
+# Mass of the Sun
+SUN_M = 1.9891 * 10**30
+
+# Mass of Mars
+MARS_M = 6.39 * 10**23 * 1000
+
+# Radius of the Sun
+SUN_RADIUS = 696_340 * 1000
+
+# Scaling factor
+SCALE = 200 / AU
+
+# Radius of the Moon
+MOON_RADIUS = 1737.4 * 1000
+
+# Mass of the Earth
+EARTH_M = 5.9742 * 10**24
+
+# Mass of the Moon
+MOON_M = 7.34767 * 10**22
+
+# Gravitational constant
+G = 6.67428e-11
+
+# Radius of the Earth
+RADIUS_EARTH = 6371 * 1000
+
+# Time step
+TIME_STEP = 60 * 20
+
+#mass of the moon
+MOON_M=7.34767 * 10**22
+
+#G constant
+G=6.67428e-11
+
+#radius of the Earth
+RADIUS_EARTH=6371*1000
+
+TIME_STEP=60*20
+
